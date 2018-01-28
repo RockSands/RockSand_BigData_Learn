17c421845c17c415d637579141b435ebd3c023c9
Administrator <Administrator@SC-201711102054>
2018-01-28 20:48:57 +0800

    样例

---
+++ RockSand_SpringBoot/src/main/java/com/controller/SampleController.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RestController Rest接口的Controller
+ * @SpringBootApplication SpringBoot的入口
+ * @author Administrator
+ *
+ */
+@RestController
+@SpringBootApplication
+public class SampleController {
+    public static void main(String[] args) {
+	/**
+	 * SpringBoot加载Main的方法一,最简操作
+	 */
+	// SpringApplication.run(SampleController.class, args);
+
+	/**
+	 * SpringBoot加载Main的方法二
+	 */
+	SpringApplication application = new SpringApplication(SampleController.class);
+	// 关闭Banner的输出
+	application.setBannerMode(Banner.Mode.OFF);
+	application.run(args);
+
+	/**
+	 * SpringBoot加载Main的方法三 使用SpringApplicationBuilder,这里不演示
+	 */
+
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+	return "Hello World!";
+    }
+}
