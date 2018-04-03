b7f517347734bdf7c29d95d838e66175918d5952
RockSands <117019049@qq.com>
2018-04-03 21:10:40 +0800

    提交

---
+++ RockSand_Mahout/src/main/java/com/BaseUserBooleanRecommender.java
@@ -0,0 +1,45 @@
+package com;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.CityBlockSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class BaseUserBooleanRecommender {
+	public static void main(String[] args) throws Exception {
+		// 准备数据 这里是电影评分数据
+		File file = new File("D:\\ratings\\running\\ratings.dat");
+		DataModel dataModel = new GenericBooleanPrefDataModel(
+				GenericBooleanPrefDataModel.toDataMap(new FileDataModel(file)));
+		// 计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
+		UserSimilarity similarity = new CityBlockSimilarity(dataModel);
+		UserNeighborhood userNeighborhood = new NearestNUserNeighborhood(50, similarity, dataModel);
+		Recommender recommender = new GenericBooleanPrefUserBasedRecommender(dataModel, userNeighborhood, similarity);
+		// 给用户ID等于5的用户推荐10部电影
+		List<RecommendedItem> recommendedItemList = recommender.recommend(14, 20);
+		// 打印推荐的结果
+		System.out.println("使用基于用户的协同过滤算法");
+		System.out.println("为用户5推荐10个商品");
+		for (RecommendedItem recommendedItem : recommendedItemList) {
+			System.out.println(recommendedItem);
+		}
+
+		// // 给用户ID等于10的用户推荐10部电影
+		// recommendedItemList = recommender.recommend(5, 10);
+		// // 打印推荐的结果
+		// System.out.println("使用基于用户的协同过滤算法");
+		// System.out.println("为用户10推荐10个商品");
+		// for (RecommendedItem recommendedItem : recommendedItemList) {
+		// System.out.println(recommendedItem);
+		// }
+	}
+}
--- RockSand_Mahout/src/main/java/com/BaseUserRecommender.java
+++ RockSand_Mahout/src/main/java/com/BaseUserRecommender.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
@@ -25,7 +25,7 @@ public class BaseUserRecommender {
 	// 将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
 	DataModel dataModel = new FileDataModel(file);
 	// 计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
-	UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+	UserSimilarity similarity = new EuclideanDistanceSimilarity(dataModel);
 	// 计算最近邻域，邻居有两种算法，基于固定数量的邻居和基于相似度的邻居，这里使用基于固定数量的邻居
 	UserNeighborhood userNeighborhood = new NearestNUserNeighborhood(100, similarity, dataModel);
 	// 构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于用户的协同过滤推荐
