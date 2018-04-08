1b83ed1e325ec94b420bb4f695548a31589c7658
RockSands <117019049@qq.com>
2018-04-08 10:22:18 +0800

    修正

--- RockSand_Mahout/src/main/java/com/BaseItemRecommender.java
+++ RockSand_Mahout/src/main/java/com/BaseItemRecommender.java
@@ -3,9 +3,11 @@ package com;
 import java.io.File;
 import java.util.List;
 
+import org.apache.mahout.cf.taste.impl.common.FastIDSet;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.UncenteredCosineSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
@@ -23,25 +25,26 @@ public class BaseItemRecommender {
 		// 将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
 		DataModel dataModel = new FileDataModel(file);
 		// 计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
-		ItemSimilarity itemSimilarity = new PearsonCorrelationSimilarity(dataModel);
+		ItemSimilarity itemSimilarity = new UncenteredCosineSimilarity(dataModel);
 		// 构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于物品的协同过滤推荐
 		GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(dataModel, itemSimilarity);
-		// 给用户ID等于5的用户推荐10个与2398相似的商品
-		List<RecommendedItem> recommendedItemList = recommender.recommendedBecause(5, 2398, 10);
+		// 给用户ID等于5的用户推荐10个商品
+		List<RecommendedItem> recommendedItemList = recommender.recommend(5, 100);
 		// 打印推荐的结果
 		System.out.println("使用基于物品的协同过滤算法");
-		System.out.println("根据用户5当前浏览的商品2398，推荐10个相似的商品");
+		System.out.println("为用户5推荐100个相似的商品");
 		for (RecommendedItem recommendedItem : recommendedItemList) {
 			System.out.println(recommendedItem);
 		}
-		long start = System.currentTimeMillis();
-		recommendedItemList = recommender.recommendedBecause(5, 34, 10);
-		// 打印推荐的结果
-		System.out.println("使用基于物品的协同过滤算法");
-		System.out.println("根据用户5当前浏览的商品34，推荐10个相似的商品");
-		for (RecommendedItem recommendedItem : recommendedItemList) {
-			System.out.println(recommendedItem);
-		}
-		System.out.println(System.currentTimeMillis() - start);
+//		FastIDSet idSet = dataModel.getItemIDsFromUser(5);
+//		LongPrimitiveIterator it = idSet.iterator();
+//		System.out.println("使用基于物品的协同过滤算法");
+//		while (it.hasNext()) {
+//			// recommendedItemList = recommender.recommendedBecause(5, it.next(), 2);
+//			recommendedItemList = recommender.mostSimilarItems(it.next(), 2);
+//			for (RecommendedItem recommendedItem : recommendedItemList) {
+//				System.out.println(recommendedItem);
+//			}
+//		}
 	}
 }
--- RockSand_Mahout/src/main/java/com/BaseUserRecommender.java
+++ RockSand_Mahout/src/main/java/com/BaseUserRecommender.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.UncenteredCosineSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
@@ -15,37 +15,53 @@ import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 /**
  * 基于用户
+ * 
  * @author Administrator
  *
  */
 public class BaseUserRecommender {
-    public static void main(String[] args) throws Exception {
-	// 准备数据 这里是电影评分数据
-	File file = new File("D:\\ratings.dat");
-	// 将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
-	DataModel dataModel = new FileDataModel(file);
-	// 计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
-	UserSimilarity similarity = new EuclideanDistanceSimilarity(dataModel);
-	// 计算最近邻域，邻居有两种算法，基于固定数量的邻居和基于相似度的邻居，这里使用基于固定数量的邻居
-	UserNeighborhood userNeighborhood = new NearestNUserNeighborhood(100, similarity, dataModel);
-	// 构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于用户的协同过滤推荐
-	Recommender recommender = new GenericUserBasedRecommender(dataModel, userNeighborhood, similarity);
-	// 给用户ID等于5的用户推荐10部电影
-	List<RecommendedItem> recommendedItemList = recommender.recommend(10, 10);
-	// 打印推荐的结果
-	System.out.println("使用基于用户的协同过滤算法");
-	System.out.println("为用户5推荐10个商品");
-	for (RecommendedItem recommendedItem : recommendedItemList) {
-	    System.out.println(recommendedItem);
-	}
+	public static void main(String[] args) throws Exception {
+		/*
+		 * 构建数据:文件
+		 */
+		// 准备数据 这里是电影评分数据
+		File file = new File("D:\\ratings\\running\\ratings.dat");
+		// 将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
+		DataModel dataModel = new FileDataModel(file);
+		/*
+		 * 构建数据:数据库
+		 */
+		// MysqlDataSource ds = new MysqlDataSource();
+		// ds.setServerName("192.168.80.138");
+		// ds.setDatabaseName("test");
+		// ds.setPort(3306);
+		// ds.setUser("root");
+		// ds.setCharacterEncoding("utf-8");
+		// ds.setPassword("123456");
+		// ds.setDatabaseName("mahout");
+		// DataModel dataModel = new MySQLJDBCDataModel(ds);
+		// 计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
+		UserSimilarity similarity = new UncenteredCosineSimilarity(dataModel);
+		// 计算最近邻域，邻居有两种算法，基于固定数量的邻居和基于相似度的邻居，这里使用基于固定数量的邻居
+		UserNeighborhood userNeighborhood = new NearestNUserNeighborhood(100, similarity, dataModel);
+		// 构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于用户的协同过滤推荐
+		Recommender recommender = new GenericUserBasedRecommender(dataModel, userNeighborhood, similarity);
+		// 给用户ID等于5的用户推荐10部电影
+		List<RecommendedItem> recommendedItemList = recommender.recommend(5, 100);
+		// 打印推荐的结果
+		System.out.println("使用基于用户的协同过滤算法");
+		System.out.println("为用户5推荐10个商品");
+		for (RecommendedItem recommendedItem : recommendedItemList) {
+			System.out.println(recommendedItem);
+		}
 
-	// 给用户ID等于10的用户推荐10部电影
-	recommendedItemList = recommender.recommend(5, 10);
-	// 打印推荐的结果
-	System.out.println("使用基于用户的协同过滤算法");
-	System.out.println("为用户10推荐10个商品");
-	for (RecommendedItem recommendedItem : recommendedItemList) {
-	    System.out.println(recommendedItem);
+		// 给用户ID等于10的用户推荐10部电影
+//		recommendedItemList = recommender.recommend(5, 10);
+//		// 打印推荐的结果
+//		System.out.println("使用基于用户的协同过滤算法");
+//		System.out.println("为用户10推荐10个商品");
+//		for (RecommendedItem recommendedItem : recommendedItemList) {
+//			System.out.println(recommendedItem);
+//		}
 	}
-    }
 }
