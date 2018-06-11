f21572f3f6cd29bf6e45630a46d19106b9ce6d73
RockSands <117019049@qq.com>
2018-06-11 17:57:14 +0800

    修正

--- RockSand_Hadoop/src/main/java/hadoop/definitive/guide/chapter000/MaxTemperatureMapper.java
+++ RockSand_Hadoop/src/main/java/hadoop/definitive/guide/chapter000/MaxTemperatureMapper.java
@@ -1,21 +1,55 @@
 package hadoop.definitive.guide.chapter000;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Mapper;
 
 /**
  * Hadoop权威指南样例代码 - 温度最大计算的mapper
  * 
- * Mapper是Hadoop的Mapper接口,四个参数分别定义了:输入Key,输入Value,输出Text,输出Value
+ * Mapper是Hadoop的Mapper接口,四个参数分别定义了:输入Key,输入Value,输出Key,输出Value
+ * 
+ * Mapper的方法中run方法是入口方法
  * 
  * @author Administrator
  *
  */
 public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+	
+	private long startTime;
+
+	/**
+	 * 在任务启动前执行一次(仅一次),可以实现来完成一些预备动作
+	 */
+	protected void setup(Context context) throws IOException, InterruptedException {
+		/*
+		 * Hadoop对于输入文件切割的对象
+		 */
+		InputSplit split = context.getInputSplit();
+		// 切割长度
+		long length = split.getLength();
+		// 获取本地地址
+		InetAddress address = InetAddress.getLocalHost();
+		// 获取主机名称
+		String hostName = address.getHostName();
+		// 获取IP值
+		String ip = address.getHostAddress();
+		startTime = System.currentTimeMillis();
+		System.out.println("MaxTemperatureMapper excute local" + hostName + ":" + ip + " split:" + length);
+	}
+
+	/**
+	 * 在任务完成后执行一次(仅一次),可以实现来完成一些清理动作
+	 */
+	protected void cleanup(Context context) throws IOException, InterruptedException {
+		System.out.println("MaxTemperatureMapper excute finished:" + (System.currentTimeMillis() - startTime)/1000);
+	}
+
 	/*
 	 * key,输入Key Value,输入Value Context,Hadoop的环境变量
 	 * 
