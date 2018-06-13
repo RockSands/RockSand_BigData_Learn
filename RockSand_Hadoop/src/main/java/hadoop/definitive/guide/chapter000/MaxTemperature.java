f76a191855f63a888243b37558da18cc3fcce299
RockSands <117019049@qq.com>
2018-06-13 09:58:40 +0800

    修正逻辑

--- RockSand_Hadoop/src/main/java/hadoop/definitive/guide/chapter000/MaxTemperature.java
+++ RockSand_Hadoop/src/main/java/hadoop/definitive/guide/chapter000/MaxTemperature.java
@@ -13,13 +13,14 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /**
  * 温度运行Main函数
+ * 
  * @author Administrator
  *
  */
 public class MaxTemperature {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-		if(args.length != 2){
+		if (args.length != 2) {
 			System.err.println("参数数量不足!");
 			System.exit(-1);
 		}
@@ -28,34 +29,36 @@ public class MaxTemperature {
 		 */
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(conf);
+		// 删除output
 		Path outpath = new Path("/output");
-		if(fs.exists(outpath)) {
-			fs.delete(outpath,false);
-		}
+		fs.deleteOnExit(outpath);
+		// 删除tmp
+		Path tmppath = new Path("/tmp");
+		fs.deleteOnExit(tmppath);
 		// 新建Job
 		Job job = Job.getInstance();
 		// 任务为本身,Hadoop会将任务打包成Jar
 		job.setJarByClass(MaxTemperature.class);
 		job.setJobName("Max Temperature");
-		
+
 		// 设定这个Job的输入文件
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		// 设定这个Job的输出文件
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
 		// 定义Mapper和Reducer
 		job.setMapperClass(MaxTemperatureMapper.class);
 		job.setCombinerClass(MaxTemperatureReducer.class);
 		job.setReducerClass(MaxTemperatureReducer.class);
-		
+
 		// 定义Job输出的类型
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(DoubleWritable.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(DoubleWritable.class);
-		
+
 		// 退出
-		System.exit(job.waitForCompletion(true) ? 0: 1);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
 }
